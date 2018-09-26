f556f7041208e7250db192d2c845ab783b42c45a
Vilhelm Falkenmark <vilhelmfalkenmark@gmail.com>
2018-09-26 13:28:54 +0200

    added MovieDetails as subroute

---
+++ src/entrypoints/MovieDetails/MoviesDetails.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const MovieDetails = ({}) => {
+ 
+  return (
+    <div>
+      <h1>VÄLKOMMEN TILL MER INFO OM FILM</h1>
+    
+    </div>
+  );
+};
+
+export default MovieDetails;
